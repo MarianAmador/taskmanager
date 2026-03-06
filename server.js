57ba8df10c680c127f5045c371e981f1fadbbc4e
Marian Amador <142361216+MarianAmador@users.noreply.github.com>
2026-03-05 22:38:13 -0700

    Serve index.html for root GET requests
    Added functionality to serve index.html on GET requests to the root URL.

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 const { handleRoutes } = require("./routes");
 
 const server = http.createServer((req, res) => {
+
+  if (req.url === "/" && req.method === "GET") {
+    const filePath = path.join(__dirname, "index.html");
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end("Error cargando HTML");
+        return;
+      }
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+
+    return;
+  }
+
   handleRoutes(req, res);
 });
 
