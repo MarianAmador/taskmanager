73ea2a6f89a3ee6e4f9baff6ed4a72c4f110464a
Marian Amador <142361216+MarianAmador@users.noreply.github.com>
2026-03-05 22:28:53 -0700

    refactor: mostrar html

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 const { handleRoutes } = require("./routes");
 
 const server = http.createServer((req, res) => {
+
+  if (req.url === "/") {
+    const filePath = path.join(__dirname, "index.html");
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end("Error cargando el HTML");
+        return;
+      }
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+
+    return;
+  }
+
   handleRoutes(req, res);
 });
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log(`✅ API corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
