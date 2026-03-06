19afd7f06ef726bebae671ef8191baad5bcebf83
Marian Amador <142361216+MarianAmador@users.noreply.github.com>
2026-03-05 22:47:37 -0700

    Delete: eliminar la carpeta

--- task-manager/server.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-const { handleRoutes } = require("./routes");
-
-const server = http.createServer((req, res) => {
-  handleRoutes(req, res);
-});
-
-server.listen(3000, () => console.log("✅ API corriendo en http://localhost:3000"));
